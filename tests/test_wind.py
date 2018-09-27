5e590a8e58b4e6c594ed240f7cd321ab4fe92068
Carles Sala <carles@pythiac.com>
2018-09-27 16:29:19 +0200

    First Commit

---
+++ tests/test_wind.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `wind` package."""
+
+import unittest
+
+from wind import wind
+
+
+class TestWind(unittest.TestCase):
+    """Tests for `wind` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
+
+    def test_000_something(self):
+        """Test something."""
+        self.assertTrue(True)
