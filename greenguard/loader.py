6a7a5b2631efe2bb717a6a69c1444fe4b3179a19
Carles Sala <carles@pythiac.com>
2019-04-18 18:13:42 +0200

    Remove unused code

--- greenguard/loader.py
+++ greenguard/loader.py
@@ -8,48 +8,6 @@ import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 
-DATA_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    'data'
-)
-BUCKET = 'd3-ai-greenguard'
-S3_URL = 'https://{}.s3.amazonaws.com/{}.csv'
-
-
-def download(name):
-    """Load the CSV with the given name from S3.
-
-    If the CSV has never been loaded before, it will be downloaded
-    from the [d3-ai-orion bucket](https://d3-ai-orion.s3.amazonaws.com)
-    and then cached inside the `data` folder, within the `orion` package
-    directory, and then returned.
-
-    Otherwise, if it has been downloaded and cached before, it will be directly
-    loaded from the `orion/data` folder without contacting S3.
-
-    Args:
-        name (str): Name of the CSV to load.
-
-    Returns:
-        If no test_size is given, a single pandas.DataFrame is returned containing all
-        the data. If test_size is given, a tuple containing one pandas.DataFrame for
-        the train split and another one for the test split is returned.
-    """
-
-    filename = os.path.join(DATA_PATH, name + '.csv')
-    if os.path.exists(filename):
-        data = pd.read_csv(filename)
-
-    else:
-        url = S3_URL.format(BUCKET, name)
-
-        LOGGER.debug('Downloading CSV %s from %s', name, url)
-        os.makedirs(DATA_PATH, exist_ok=True)
-        data = pd.read_csv(url)
-        data.to_csv(filename, index=False)
-
-    return data
-
 
 class GreenGuardLoader(object):
 
