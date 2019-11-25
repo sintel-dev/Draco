b5f8c234b6b697d7f37b4ca066d3fc4d90b14013
Carles Sala <carles@pythiac.com>
2019-11-25 13:43:31 +0100

    Remove unused import

--- greenguard/__init__.py
+++ greenguard/__init__.py
@@ -9,7 +9,6 @@ __version__ = '0.1.1-dev'
 import os
 
 from greenguard.data import extract_readings, make_targets
-from greenguard.loader import load_demo
 from greenguard.pipeline import GreenGuardPipeline, get_pipelines
 
 _BASE_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +18,6 @@ MLBLOCKS_PIPELINES = os.path.join(_BASE_PATH, 'pipelines')
 __all__ = (
     'GreenGuardPipeline',
     'get_pipelines',
-    'load_demo',
     'extract_readings',
     'make_targets'
 )
