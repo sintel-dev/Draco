a8d41b735bf5a8c900ab4ce74baff7644d2d4d96
Carles Sala <carles@pythiac.com>
2019-11-29 12:01:42 +0100

    Handle pipeline init_params properly and add pipeline repr

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -5,22 +5,26 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+from mlblocks.discovery import find_pipelines, load_pipeline
+
 from greenguard.pipeline import GreenGuardPipeline
 
 
 class TestGreenGuardPipeline(TestCase):
     """Tests for `TimeSeriesClassifier`."""
 
+    PIPELINE_NAME = find_pipelines()[0]
+
     @patch('greenguard.pipeline.MLPipeline')
     def test_fit(self, pipeline_class_mock):
         """fit prepare the pipeline to make predictions based on the given data."""
         # Run
-        instance = GreenGuardPipeline('pipeline_name', 'accuracy')
+        instance = GreenGuardPipeline(self.PIPELINE_NAME, 'accuracy')
         instance.fit('an_X', 'a_y', 'readings')
 
         # Asserts
         pipeline_mock = pipeline_class_mock.return_value
-        pipeline_class_mock.assert_called_once_with('pipeline_name')
+        pipeline_class_mock.assert_called_once_with(load_pipeline(self.PIPELINE_NAME))
         assert instance._pipeline == pipeline_mock
 
         pipeline_mock.fit.assert_called_once_with('an_X', 'a_y', readings='readings')
@@ -31,7 +35,7 @@ class TestGreenGuardPipeline(TestCase):
     def test_predict(self, pipeline_mock):
         """predict produces results using the pipeline."""
         # Run
-        instance = GreenGuardPipeline('pipeline_name', 'accuracy')
+        instance = GreenGuardPipeline(self.PIPELINE_NAME, 'accuracy')
         instance.fitted = True
         instance.predict('an_X', 'readings')
 
