5de0680cbf4fc059bd4fa9fea49ac7200280edb0
Carles Sala <carles@pythiac.com>
2020-02-11 10:57:16 -0500

    Bump version: 0.2.0.dev0 → 0.2.0.dev1

--- setup.py
+++ setup.py
@@ -103,6 +103,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/GreenGuard',
-    version='0.2.0.dev0',
+    version='0.2.0.dev1',
     zip_safe=False,
 )
