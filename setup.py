dba471652ad7e1833732e7531b91d7cb6698d8bd
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-06-16 11:06:54 +0200

    Bump version: 0.2.1 → 0.2.2.dev0

--- setup.py
+++ setup.py
@@ -105,6 +105,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/GreenGuard',
-    version='0.2.1',
+    version='0.2.2.dev0',
     zip_safe=False,
 )
