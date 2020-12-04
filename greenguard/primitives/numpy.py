3b02b1ad026165e5f4ff82c624d62626b4da610d
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-12-04 12:49:54 +0100

    Use numpy.take

--- greenguard/primitives/numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def slice_by_index(array, axes=None, index=0):
-    """Transpose and return the value at the given key."""
-    array = np.ndarray.transpose(array, axes)
-    return np.ndarray.__getitem__(array, 1)
