e360694afa0a4e7f808b176a362c11f64f7e7d7b
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2020-12-07 17:56:43 +0100

    Add false positive rate metric (#56)
    * Add integration tests
    
    * Add FPR metric
    
    * Fix tests
    
    * Fix lint py37
    
    * Add dosctring

--- greenguard/metrics.py
+++ greenguard/metrics.py
@@ -1,11 +1,47 @@
 # -*- coding: utf-8 -*-
+import logging
 
+import numpy as np
 from sklearn.metrics import (
-    accuracy_score, f1_score, mean_absolute_error, mean_squared_error, r2_score)
+    accuracy_score, f1_score, mean_absolute_error, mean_squared_error, roc_curve, r2_score)
+
+LOGGER = logging.getLogger(__name__)
 
 
 def f1_macro(exp, obs):
     return f1_score(exp, obs, average='macro')
+
+
+def fpr_score(ground_truth, probabilities, tpr=1):
+    """Compute the False Positive Rate associated with the given True Positive Rate.
+
+    This metric computes the False Positive Rate that needs to be assumed in order
+    to achieve the desired True Positive Rate.
+    The metric is computed by finding the minimum necessary threshold to ensure
+    that the TPR is satisfied and then computing the associated FPR. The final output
+    is 1 minus the found FPR to produce a maximization score between 0 and 1.
+
+    Args:
+        ground_truth (numpy.ndarray):
+            ``numpy.ndarray`` of the known values for the given predictions.
+        probabilities (numpy.ndarray):
+            ``numpy.ndarray`` with the generated predictions in probability.
+        tpr (float):
+            ``float`` value representing the percentage of True Positive Rate
+            to be satisfied.
+
+    Returns:
+        float:
+            Value between 0 and 1, where bigger is better.
+    """
+    roc_fpr, roc_tpr, roc_threshold = roc_curve(ground_truth, probabilities, pos_label=1)
+    try:
+        index = np.where(roc_tpr >= tpr)[0][0]
+    except:
+        LOGGER.warn('Could not find a threshold that satisfies the requested True Positive Rate')
+        index = -1
+
+    return 1 - roc_fpr[index]
 
 
 METRICS = {
@@ -14,5 +50,6 @@ METRICS = {
     'f1_macro': (f1_macro, False),
     'r2': (r2_score, False),
     'mse': (mean_squared_error, True),
-    'mae': (mean_absolute_error, True)
+    'mae': (mean_absolute_error, True),
+    'fpr_score': (fpr_score, False)
 }
