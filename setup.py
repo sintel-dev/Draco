fe9123d3e951b33c07fe21fedbeb74c26f74a618
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-01-14 21:21:32 +0100

    Fix dependancy requirement

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ install_requires = [
     'pandas>=1,<2',
     'dask>=2.6.0,<3',
     'distributed>=2.6.0,<3',
+    'h5py<2.11.0,>=2.10.0',  # fix tensorflow requirement
     'Keras>=2.4',
     'tabulate>=0.8.3,<0.9',
     'xlsxwriter>=1.3.6<1.4',
