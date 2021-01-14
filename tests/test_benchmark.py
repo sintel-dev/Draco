b3ad5d77eae66c3ecd7512195410d02b81929b07
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-01-14 15:32:51 +0100

    Fix test

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,5 +1,5 @@
 """Tests for `greenguard.benchmark` module."""
-from sklearn.metrics import f1_score
+import numpy as np
 
 from greenguard.benchmark import evaluate_templates
 from greenguard.demo import load_demo
@@ -8,7 +8,7 @@ from greenguard.demo import load_demo
 def test_predict():
     # setup
     templates = [
-        'unstack_lstm_timeseries_classifier'
+        'probability.unstack_lstm_timeseries_classifier'
     ]
 
     window_size_rule = [
@@ -16,7 +16,7 @@ def test_predict():
     ]
 
     target_times, readings = load_demo()
-    target_times = target_times.head(10)
+    target_times = target_times.head(40)
     readings = readings.head(100)
 
     # run
@@ -25,7 +25,6 @@ def test_predict():
         readings=readings,
         templates=templates,
         window_size_rule=window_size_rule,
-        metric=f1_score,
         tuning_iterations=1,
         cv_splits=2
     )
@@ -40,29 +39,39 @@ def test_predict():
         'default_cv',
         'tuned_cv',
         'tuned_test',
-        'metric',
+        'tuning_metric',
+        'tuning_metric_kwargs',
         'fit_predict_time',
         'default_cv_time',
         'average_cv_time',
         'total_time',
-        'status'
+        'status',
+        'accuracy_threshold/0.5',
+        'f1_threshold/0.5',
+        'fpr_threshold/0.5',
+        'tpr_threshold/0.5',
     ]
 
     expected_dtypes = [
-        'object',
-        'object',
-        'object',
-        'object',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'float64',
-        'object',
+        np.dtype('O'),
+        np.dtype('O'),
+        np.dtype('O'),
+        np.dtype('O'),
+        np.dtype('float64'),
+        np.dtype('float64'),
+        np.dtype('float64'),
+        np.dtype('float64'),
+        np.dtype('O'),
+        np.dtype('O'),
+        np.dtype('<m8[ns]'),
+        np.dtype('<m8[ns]'),
+        np.dtype('<m8[ns]'),
+        np.dtype('<m8[ns]'),
+        np.dtype('O'),
+        np.dtype('float64'),
+        np.dtype('float64'),
+        np.dtype('float64'),
+        np.dtype('float64')
     ]
 
     assert (scores_df.columns.to_list() == expected_columns)
