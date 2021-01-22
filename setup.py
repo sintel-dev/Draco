c730e1e1d6cbca04a355e6ae08fba045e5cd0a19
Carles Sala <carles@pythiac.com>
2021-01-22 18:07:48 +0100

    Revert 1.0.0 error release

--- setup.py
+++ setup.py
@@ -115,6 +115,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/GreenGuard',
-    version='1.0.1.dev0',
+    version='0.3.0.dev0',
     zip_safe=False,
 )
