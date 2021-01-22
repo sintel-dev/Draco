23216be0272572d8d2567e894e11e3466d10b23d
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-01-22 15:33:29 +0100

    Add release notes for 0.3.0

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description='AutoML for Renewable Energy Industries.',
     entry_points={
