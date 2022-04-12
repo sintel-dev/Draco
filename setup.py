9aa2b2203fd8c24d965c15c98d6409ead33f5ec0
sarahmish <sarahalnegheimish@gmail.com>
2022-04-12 03:28:19 -0400

    Bump version: 0.2.0.dev0 → 0.2.0

--- setup.py
+++ setup.py
@@ -115,6 +115,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/Draco',
-    version='0.2.0.dev0',
+    version='0.2.0',
     zip_safe=False,
 )
